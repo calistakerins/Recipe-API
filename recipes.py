66fe15646d96ca8dc262766fd320a4b32b944899
Anna Rosenberg <58891612+annarosenberg-lab@users.noreply.github.com>
2023-04-26 14:19:37 -0700

    Update recipes.py
    Added GET and POST labels. Updated favorite_recipe and add_recipe POST endpoints to follow the POST definition shown in assignment 3.

--- recipes.py
+++ recipes.py
@@ -62,29 +62,63 @@ def list_recipe(recipe: str = "",
     return
 
 
-def add_recipe(recipe: str,
-    cuisine: str,
-    meal_type: str,
-    ingredients: list[dict],
-    time: int):
+
+class RecipeJson(BaseModel):
+    recipe_name: string
+    cusine_type: string
+    meal_type: string
+    calories: int
+    prep_time: int
+    num_ingredients: int
+    recipe_url: string
+    
+class IngredientJson(BaseModel):
+    ingredient_id: int
+    unit_type: string
+    amount: int
+        
+class QuantitiesJson(BaseModel):
+    ingredientQuantities = List[IngredientJson]
+
+
+@router.post("/recipes/{recipe_id}", tags=["recipes"])
+def add_recipe(recipe: RecipeJson, ingredient_quantities:QuantitiesJson):
     """
     This endpoint will allow users to add their own recipes to the API. A recipie will consist of:
-    * `recipe`: The name of the recipe.
-    * `cuisine`: The cuisine that the recipe is from.
+    * `recipe_name`: The name of the recipe.
+    * `cuisine_type`: The cuisine that the recipe is from.
     * `meal_type`: The meal type that the recipe is from.
-    * `ingredients`: The listed ingredients and amounts that are needed to make the recipe.
-    * `time`: The total time it takes to make the recipe.
+    * `calories`: total calories in the recipe
+    * `prep_time`: The total time it takes to make the recipe.
+    * `num_ingredients`: The total number of ingredients the recipe.
+    * `recipe_url`: url link to the recipe.
+    
+    This endpoint will also require users to specify the ingredient qauntities required
+    to make the recipe. For each ingredient in the recipe, the user must specfiy:
+     * `ingredient_id`: The id of the ingredient.
+     * `unit_type`: the unit the recipe is measured in.
+     * `amount`: the amount of ingredient used in recipe.
+
+    To add a new recipe, all ingredients in the recipe must already but in the database.
+    If an ingredient is not, this enpoint will throw an error and ask users to add
+    the new ingredient first. 
+
+    This enpoint will return the id of the resulting recipe created
     """
     return 
 
+@router.post("/recipes/{recipe_id}", tags=["recipes"])
 def favorite_recipe(recipe_id: int,
     ):
     """
     This endpoint will allow users to add existing recipes to their favorites list. 
     It will write the recipe_id to the favorite_recipes database.
+    
+    This endpoint will return the recipe id that was added to the favorited_recipes.
     """
     return
 
+@router.get("/favorite_recipes/", tags=["favorite_recipes"])
 def list_favorite_recipes(limit: int = Query(50, ge=1, le=250),
     offset: int = Query(0, ge=0),
     sort: recipe_sort_options = recipe_sort_options.recipe
