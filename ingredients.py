843a1cb4b40333768ff3ec8f6de9e4996c315a93
mtan22 <76962221+mtan22@users.noreply.github.com>
2023-04-26 16:11:33 -0700

    Update ingredients.py

--- ingredients.py
+++ ingredients.py
@@ -31,8 +31,8 @@ class IngredientJson(BaseModel):
 def add_ingredient(ingredient: IngredientJson):
     """
     This endpoint adds a single ingredient. A new ingredient is represented by its name,
-    and its cost. When added, its num_recipies attribute will be intialized to 0, as no can recipes
-    contain this ingreient yet because it is new. 
+    and its cost. When added, its num_recipes attribute will be intialized to 0, as no can recipes
+    contain this ingredient yet because it is new. 
     
     
     This endpoint will return the new id of the ingredient created. 
@@ -40,6 +40,34 @@ def add_ingredient(ingredient: IngredientJson):
     
     
     return {"ingredient_id": ingredient_id}
-    
+
+class character_sort_options(str, Enum):
+    ingredient = "ingredient"
+
+@router.get("/ingredients/", tags=["ingredients"])
+def list_ingredients(
+    name: str = "",
+    limit: int = Query(50, ge=1, le=250),
+    offset: int = Query(0, ge=0),
+    sort: character_sort_options = character_sort_options.character,):
+    """
+    This endpoint returns a list of ingredients. For each ingredient it returns:
+    * `ingr_id`: the internal id of the ingredient. Can be used to query the
+      `/ingredients/{ingr_id}` endpoint.
+    * `ingredient_name`: The name of the ingredient.
+    * `recipes`: The list of recipes that include this ingredient.
+
+    You can filter for characters whose name contains a string by using the
+    `name` query parameter.
+
+    You can also sort the results by using the `sort` query parameter:
+    * `ingredient` - Sort by ingredient name alphabetically.
+
+    The `limit` and `offset` query
+    parameters are used for pagination. The `limit` query parameter specifies the
+    maximum number of results to return. The `offset` query parameter specifies the
+    number of results to skip before returning results.
+    """
+    return json
     
 
