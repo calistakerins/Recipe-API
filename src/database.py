2f419b8c7d2e2ed694547a171962caac7258466f
Anna Rosenberg <58891612+annarosenberg-lab@users.noreply.github.com>
2023-05-07 16:25:02 -0700

    Update database.py

--- src/database.py
+++ src/database.py
@@ -1,5 +1,4 @@
 import csv
-from src.datatypes import Character, Movie, Conversation, Line
 import os
 import io
 from supabase import Client, create_client
@@ -36,7 +35,10 @@ conn = engine.connect()
 
 metadata_obj = sqlalchemy.MetaData()
 
-conversations = sqlalchemy.Table("conversations", metadata_obj, autoload_with=engine)
-lines = sqlalchemy.Table("lines", metadata_obj, autoload_with=engine)
-movies = sqlalchemy.Table("movies", metadata_obj, autoload_with=engine)
-characters = sqlalchemy.Table("characters", metadata_obj, autoload_with=engine)
+conversations = sqlalchemy.Table("recipes", metadata_obj, autoload_with=engine)
+lines = sqlalchemy.Table("ingredients", metadata_obj, autoload_with=engine)
+movies = sqlalchemy.Table("ingredient_quantities", metadata_obj, autoload_with=engine)
+characters = sqlalchemy.Table("cuisine_type", metadata_obj, autoload_with=engine)
+characters = sqlalchemy.Table("meal_type", metadata_obj, autoload_with=engine)
+characters = sqlalchemy.Table("favorited_recipes", metadata_obj, autoload_with=engine)
+characters = sqlalchemy.Table("user", metadata_obj, autoload_with=engine)
