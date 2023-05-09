a6eb6a39d99e48c5e69b6c011a0cb72e01da3803
Anna Rosenberg <58891612+annarosenberg-lab@users.noreply.github.com>
2023-05-08 23:58:30 -0700

    Add files via upload

---
+++ test/test_recipes.py
@@ -0,0 +1,29 @@
+from fastapi.testclient import TestClient
+
+from src.api.server import app
+
+import json
+
+client = TestClient(app)
+
+def test_get_recipe():
+    response = client.get("/recipes/1")
+    assert response.status_code == 200
+
+    with open("test/recipes/1.json", encoding="utf-8") as f:
+        assert response.json() == json.load(f)
+
+def test_get_recipe2():
+    response = client.get("/recipes/1000")
+    assert response.status_code == 404
+
+def test_list_recipes():
+    response = client.get("/recipes/?recipe=Chorizo%20Street%20Tacos&limit=50&offset=0&sort=recipe")
+    assert response.status_code == 200
+
+    with open("test/recipes/list.json", encoding="utf-8") as f:
+        assert response.json() == json.load(f)
+
+def test_list_recipes2():
+    response = client.get("/recipes/?recipe_name=chicken")
+    assert response.status_code == 404
