fc39ea95dc0afd4a7f437497f6987bce78db2c21
Anna Rosenberg <58891612+annarosenberg-lab@users.noreply.github.com>
2023-05-22 00:38:08 -0700

    Create users.py

---
+++ src/api/users.py
@@ -0,0 +1,75 @@
+import os
+from typing import List
+from fastapi import APIRouter, HTTPException
+import sqlalchemy
+import hashlib
+
+
+from src import database as db
+
+from fastapi.params import Query
+
+
+router = APIRouter()
+
+
+@router.post("/users/", tags=["add_user"])
+def add_user(username: str, password: str):
+    """
+    This endpoint will allow users to create a new user. The user must provide a username
+    and password. This endpoint check to make sure no duplicate usernames can be used.
+    All passwords will be stored as hashes to protect user privacy.
+    """
+
+    salt = os.urandom(32)
+    key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+    hashed_password = salt + key
+
+    stmt = sqlalchemy.select(db.users.c.user_id).where(db.users.c.user_name == username)
+    with db.engine.begin() as conn:
+      check_valid = conn.execute(stmt)
+      if check_valid .rowcount > 0:
+        raise HTTPException(status_code=404, detail="username already exists")
+
+      conn.execute(
+              sqlalchemy.insert(db.users),
+              [
+                  {"user_name": username,
+                  "password": hashed_password.decode('latin-1')},
+              ],
+            
+      )
+
+      stmt = sqlalchemy.select(db.users.c.user_id, db.users.c.password).where(db.users.c.user_name == username)
+      result = conn.execute(stmt)
+      for row in result:
+         user_id = row.user_id
+         hashed_password = row.password
+
+    return user_id, hashed_password
+
+
+@router.get("/users/", tags=["validate_user"])
+def validate_user_login(username: str, password: str):
+   """
+    This endpoint will allow users to check if their password is valid. This endpoint 
+    will throw an error if the user does not exist, or if the passowrd is incorrect.
+    """
+
+   stmt = sqlalchemy.select(db.users.c.password).where(db.users.c.user_name == username)
+   with db.engine.begin() as conn:
+      result = conn.execute(stmt)
+      if result.rowcount == 0:
+        raise HTTPException(status_code=404, detail="user not found")
+      for row in result:
+         hashed_password = row.password
+   hashed_password = hashed_password.encode('latin-1')
+   salt =hashed_password[:32]
+   key = hashed_password[32:]
+   check_key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+
+   if key == check_key:
+        return {"message": "Password is correct"}
+   else:
+        return {"message": "Password is incorrect"}
+      
