315a75e46f25fe88a6b9bbf144109775c8d5624c
Anna Rosenberg <58891612+annarosenberg-lab@users.noreply.github.com>
2023-06-03 16:17:39 -0700

    Update database.py

--- src/database.py
+++ src/database.py
@@ -6,19 +6,7 @@ import dotenv
 from sqlalchemy import create_engine
 import sqlalchemy
 
-# # DO NOT CHANGE THIS TO BE HARDCODED. ONLY PULL FROM ENVIRONMENT VARIABLES.
-dotenv.load_dotenv()
-supabase_api_key = os.environ.get("SUPABASE_API_KEY")
-supabase_url = os.environ.get("SUPABASE_URL")
 
-if supabase_api_key is None or supabase_url is None:
-    raise Exception(
-        "You must set the SUPABASE_API_KEY and SUPABASE_URL environment variables."
-    )
-
-supabase: Client = create_client(supabase_url, supabase_api_key)
-
-sess = supabase.auth.get_session()
 
 def database_connection_url():
     dotenv.load_dotenv()
